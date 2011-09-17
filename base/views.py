a3b5e71611984542ff67be26ed237d93cfcb0f03
Bruno Rahle <gmail@brahle.com>
2011-09-17 08:10:08 -0700

    Sustav za registraciju novih korisnika

--- base/views.py
+++ base/views.py
@@ -1,1 +1,32 @@
 # Create your views here.
+
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def register(request):
+    if 'next' in request.REQUEST: 
+        next_url = request.REQUEST['next']
+    else:
+        # FIXME(brahle): url here
+        next_url = '/'
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            
+            if user is not None and user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(next_url)
+    else:
+        form = UserCreationForm()
+    return render_to_response(
+        'register.html',
+        {'form': form, 'next': next_url},
+        context_instance=RequestContext(request)
+    )
