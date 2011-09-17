0c4ac064178726ebab00615fa4817e05d1f319b8
Goran Zuzic <zuza777@gmail.com>
2011-09-17 11:55:08 -0700

    Changed MathContent to act polymorphically
    As of now, the project should install model_utils.managers

--- mathcontent/models.py
+++ mathcontent/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 from django.template.loader import render_to_string
+from model_utils.managers import InheritanceManager
+
+# TODO(gzuzic): measure the number of SQL queries made and whether can it be improved
 
 class MathContent(models.Model):
+    objects = InheritanceManager();
+
+    def __unicode__(self):
+        """Polymorphically call a deriving class member __unicode__()"""
+        return MathContent.objects.select_subclasses().get(id=self.id).__unicode__()
+
     def render(self):
-        return self.mathcontenttext.render();
+        """Polymorphically call a deriving class member render()"""
+        return MathContent.objects.select_subclasses().get(id=self.id).render()
 
 class MathContentText(MathContent):
     text = models.TextField();
