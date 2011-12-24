2aa66ccd16de18d938687fbed26463e1113a38d2
Ivica Kicic <ivicakicic@gmail.com>
2011-12-24 23:39:01 +0100

    Extended search to any tagged object

--- search/utils.py
+++ search/utils.py
@@ -7,7 +7,7 @@ from search.models import SearchCache, SearchCacheElement
 from permissions.constants import VIEW
 from task.models import Task
 
-from taggit.models import Tag
+from taggit.models import Tag, TaggedItem
 from taggit.utils import parse_tags
 
 
@@ -33,7 +33,7 @@ def update_search_cache(object, old_tags, new_tags):
     
 # recursive
 # tags SHOULD be sorted (in order to do this efficiently)
-def search_and_cache_tasks(tags, task_content_type):
+def search_and_cache(tags):
     tag_string = u','.join(tags)
     try:
         return SearchCache.objects.get(tag_string=tag_string)
@@ -42,24 +42,26 @@ def search_and_cache_tasks(tags, task_content_type):
         cache.save()
         cache.tags.set(*tags)
         
+    cache_content_type = ContentType.objects.get_for_model(SearchCache)
+        
 # TODO: change to case-insensitive
 
-    cached_tasks = Task.objects
+    cached_objects = TaggedItem.objects
     if len(tags) > 1:
-        recursion = search_and_cache_tasks(tags[:-1], task_content_type)
-        ids = SearchCacheElement.objects.filter(cache=recursion, content_type=task_content_type).values_list('object_id', flat=True)
-        cached_tasks = cached_tasks.filter(id__in=ids)
+        recursion = search_and_cache(tags[:-1])
+        ids = SearchCacheElement.objects.filter(cache=recursion).values_list('object_id', flat=True)
+        cached_objects = cached_objects.filter(object_id__in=ids)
 
-    tag_object = Tag.objects.filter(name=tags[-1])
-    cached_tasks = cached_tasks.filter(tags=tag_object)
+    tag = Tag.objects.filter(name=tags[-1])
+    # search shouldn't include itself
+    cached_objects = cached_objects.filter(tag=tag).exclude(content_type=cache_content_type)
 
-#    for task in cached_tasks:
-#        element = SearchCacheElement(content_object=task, cache=cache)
-#        element.save()
-    query = 'INSERT INTO "search_searchcacheelement" ("object_id", "content_type_id", "cache_id")\
-            SELECT "task_task"."id", %d, %d FROM "task_task" WHERE "task_task"."id" IN (%s)'        \
-            % (task_content_type.id, cache.id, u','.join([str(x) for x in cached_tasks.values_list('id', flat=True)]))
+# TODO: replace this SELECT with cached_objects query
+    query = 'INSERT INTO "search_searchcacheelement" ("object_id", "content_type_id", "cache_id")       \
+            SELECT "object_id", "content_type_id", %d FROM "taggit_taggeditem" WHERE "id" IN (%s)' %    \
+            (cache.id, ','.join((str(x) for x in cached_objects.values_list('id', flat=True))))
     print query
+
     cursor = connection.cursor()
     cursor.execute(query)
     transaction.commit_unless_managed()
@@ -77,7 +79,7 @@ def search_tasks(tags=[], none_if_blank=True, user=None, show_hidden=False):
         cache = None
     else:
         tags = sorted(tags)
-        cache = search_and_cache_tasks(tags, task_content_type)
+        cache = search_and_cache(tags)
 
     if show_hidden:
         tasks = Task.objects.for_user(user, VIEW)
