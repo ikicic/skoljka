381d0d6ad44ba3b45d5aed904427537dc8472f39
Ivica Kicic <ivicakicic@gmail.com>
2012-10-07 18:46:38 +0200

    added missing var & whitespace; variable names

--- base/static/script.js
+++ base/static/script.js
@@ -27,8 +27,8 @@ $.ajaxSetup({
 
 /* Automatically add preview button, help link and div after MathContentForm */
 $(function(){
-  mc = $('.mathcontent_text');
-  f = mc.closest('form');
+  var mc = $('.mathcontent_text');
+  var f = mc.closest('form');
   /* Preview button and help link */
   f.children('input[type=submit]').after(
       ' <button type="button" class="btn mathcontent_preview_button">Pregled</button>'
@@ -38,8 +38,8 @@ $(function(){
 
   /* Preview button, send AJAX request to convert to html (and generate necessarry .png files) */
   $('.mathcontent_preview_button').click(function(){
-    t = $('.mathcontent_text').val();
-    p = $('.mathcontent_preview');
+    var t = $('.mathcontent_text').val();
+    var p = $('.mathcontent_preview');
     p.html('Učitavanje...');
     p.attr('style', 'block');
     $.get('/ajax/mathcontent/preview/', {text: t}, function(d){
@@ -52,7 +52,7 @@ $(function(){
 /* Reply link for comments, Used for inc_post_list_small.html */
 /* MathContent View source & quote link */
 function quote(mc) {
-  s = $.trim(mc.find('.mc_viewsource_text').text());
+  var s = $.trim(mc.find('.mc_viewsource_text').text());
   $('#id_text').val('\n\n[quote]' + s + '[/quote]');  
 };
 
@@ -68,12 +68,12 @@ function set_reply(id) {
 $(function(){
   /* Post reply */
   $('.post_reply').click(function(){
-    id = $(this).attr('id').substr(2)
+    var id = $(this).attr('id').substr(2)
 
     set_reply(id);
     quote($(this).closest('.post').find('.mc'));
 
-    a = $('#reply_to_info a:first')
+    var a = $('#reply_to_info a:first')
     a.html('komentar #' + id)
     a.attr('href', '#post' + id)
   });
@@ -114,7 +114,7 @@ $(function(){
   
   /* Add tag */
   $('#tt_add').keypress(function(event){
-   name = $(this).val()
+   var name = $(this).val()
    if (event.which != 13 || name.length == 0) return;
    
    $(this).val('');
@@ -123,7 +123,7 @@ $(function(){
      name: name,
      task: $('#tt_text').data('tag').parent().attr('data-task')
    }, function(data){
-     ok = data == '1'
+     var ok = data == '1'
      $('#tt_info').html(ok ? 'Spremljeno' : 'Greška');
      if (ok) $('#tt_text').data('tag').parent().append(' | ' + name);
    });
@@ -133,7 +133,7 @@ $(function(){
   $('#tt_delete').click(function(e){
     e.preventDefault();
     
-    tag = $('#tt_text').data('tag');
+    var tag = $('#tt_text').data('tag');
     $('#tt_info').html('...');
     
     $.post('/ajax/tag/delete/', {
@@ -144,7 +144,7 @@ $(function(){
           $('#tt_text').data('tooltip').hide();
           tag.remove();
         } else {
-          msg = data == '0' ? 'Nemaš prava.' : 'Greška!';
+          var msg = data == '0' ? 'Nemaš prava.' : 'Greška!';
           $('#tt_info').html(msg);
         }
     });
@@ -156,7 +156,7 @@ $(function(){
   vote_func = function(e,v){
     e.preventDefault();
     
-    tag=$('#tt_text').data('tag');
+    var tag = $('#tt_text').data('tag');
     $('#tt_info').html('...');
     
     $.post('/ajax/tag/vote/', {
@@ -171,15 +171,15 @@ $(function(){
     // TODO: switch to JQuery 1.5+
     // .error(function(){$('#tt_info').html('Error');});
   }
-  $('#tt_plus').click(function(e){vote_func(e,1)});
-  $('#tt_minus').click(function(e){vote_func(e,-1)});
+  $('#tt_plus').click(function(e){vote_func(e,1);});
+  $('#tt_minus').click(function(e){vote_func(e,-1);});
 
   $('.tag_list a').tooltip({
     tip: '#tag_tooltip',
     position: 'bottom center',
     onBeforeShow: function(){
-      tag = this.getTrigger();
-      x = $('#tt_text');
+      var tag = this.getTrigger();
+      var x = $('#tt_text');
       x.html(tag.attr('data-votes'));
       x.data('tag', tag);
       x.data('tooltip', this);
@@ -211,8 +211,8 @@ $(function(){
 $(function(){
   $('span.task_submitted').hover(
     function() {
-      id = $(this).attr('data-solution');
-      $(this).append(' <a id="view_solution123" href="/solution/'+id+'/"><i class="icon-search"></i></a>');
+      var id = $(this).attr('data-solution');
+      $(this).append(' <a id="view_solution123" href="/solution/' + id + '/"><i class="icon-search"></i></a>');
     }, function(){
       $('#view_solution123').remove();
     });
@@ -231,9 +231,9 @@ $(function() {
 function jquery_rating_submit(name, url) {
   $(function() {
     $('.' + name + '-star').rating({
-      callback: function(v, l) {
-        data = {};
-        data[name] = typeof v == 'undefined' ? 0 : v;
+      callback: function(value, link) {
+        var data = {};
+        data[name] = typeof value == 'undefined' ? 0 : value;
         $.post(url, data, function(x) {
           $('#' + name + '-value').html(x);
         });
@@ -245,16 +245,16 @@ function jquery_rating_submit(name, url) {
 /* Email decode */
 /* email = even position characters + reversed(odd) (0-based) */
 function decode_email(e) {
-  output = '';
-  for (i = 0; i < e.length; ++i)
+  var output = '';
+  for (var i = 0; i < e.length; ++i)
     output += i % 2 == 0 ? e[i / 2] : e[e.length - (i + 1) / 2];
   return output;
 };
 
 $(function() {
   $('.imejl').each(function() {
-    a = $(this);
-    email = decode_email(a.attr('data-address'));
+    var a = $(this);
+    var email = decode_email(a.attr('data-address'));
     if (a.html().length == 0)
       a.html(email);
     a.attr('href', 'mailto:' + email);
